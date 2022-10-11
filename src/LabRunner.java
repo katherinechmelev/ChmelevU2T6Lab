41f66e8010ed7c91360de1f6fb73fe59fbf6575f
Katherine <katherinechmelev@gmail.com>
2022-10-10 22:16:23 -0400

    finished

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,6 +1,27 @@
 public class LabRunner {
     public static void main(String args []){
-        System.out.println(“-\\_(\"/)_/-");
+        int a = 1988;
+
+        int b = 1990;
+
+
+
+        String claim = " that the world’s athletes " +
+
+                "competed in Olympic Games in ";
+
+
+
+        String s = "It is " + true + claim + a +
+
+                " but " + false + claim + b + ".";
+
+
+
+        System.out.println(s);
+
+
+
 
 
 
